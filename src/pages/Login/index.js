e347363ecd88356442c1902069c2d84e3d7dfe83
audisiofilho <obuzua46@gmail.com>
2021-12-16 18:58:12 -0300

    Primeira Etapa: Fluxo de login/cadastro

---
+++ src/pages/Login/index.js
@@ -0,0 +1,118 @@
+import React, {useState, useContext} from 'react';
+import {Text, View, ActivityIndicator} from 'react-native';
+
+import {AuthContext} from '../../contexts/auth';
+
+import {
+  Container,
+  Title,
+  Input,
+  Button,
+  ButtonText,
+  SignUpButton,
+  SignUpText,
+} from './styles';
+
+export default function Login() {
+  const {signUp, signIn, loadingAuth} = useContext(AuthContext);
+
+  const [login, setLogin] = useState(true);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function toggleLogin() {
+    setLogin(!login);
+    setName('');
+    setEmail('');
+    setPassword('');
+  }
+
+  function handleLogin() {
+    if (email === '' || password === '') {
+      console.log('preencha todos os campos');
+      return;
+    }
+    signIn(email, password);
+  }
+
+  function handleSignUp() {
+    if (name === '' || email === '' || password === '') {
+      console.log('preencha todos os campos');
+      return;
+    }
+    //Cadastrando Usuario.
+    signUp(email, password, name);
+  }
+
+  if (login) {
+    return (
+      <Container>
+        <Title>
+          Awesome<Text style={{color: '#e52246'}}>Posts</Text>
+        </Title>
+
+        <Input
+          value={email}
+          onChangeText={text => setEmail(text)}
+          placeholder="email@email.com"
+        />
+        <Input
+          value={password}
+          onChangeText={text => setPassword(text)}
+          placeholder="*********"
+          secureTextEntry={true}
+        />
+
+        <Button onPress={handleLogin}>
+          {loadingAuth ? (
+            <ActivityIndicator size={20} color="#FFF" />
+          ) : (
+            <ButtonText>Acessar</ButtonText>
+          )}
+        </Button>
+
+        <SignUpButton onPress={() => toggleLogin()}>
+          <SignUpText>Criar uma conta.</SignUpText>
+        </SignUpButton>
+      </Container>
+    );
+  }
+
+  return (
+    <Container>
+      <Title>
+        Awesome<Text style={{color: '#e52246'}}>Posts</Text>
+      </Title>
+
+      <Input
+        value={name}
+        onChangeText={text => setName(text)}
+        placeholder="Usuario"
+      />
+      <Input
+        value={email}
+        onChangeText={text => setEmail(text)}
+        placeholder="email@email.com"
+      />
+      <Input
+        value={password}
+        onChangeText={text => setPassword(text)}
+        placeholder="*********"
+        secureTextEntry={true}
+      />
+
+      <Button onPress={handleSignUp}>
+        {loadingAuth ? (
+          <ActivityIndicator size={20} color="#FFF" />
+        ) : (
+          <ButtonText>Cadastrar</ButtonText>
+        )}
+      </Button>
+
+      <SignUpButton onPress={() => toggleLogin()}>
+        <SignUpText>Já tenho uma conta.</SignUpText>
+      </SignUpButton>
+    </Container>
+  );
+}
