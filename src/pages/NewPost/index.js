356f8cfa79780d7116bbeeafd1f9f990cae308d9
audisiofilho <obuzua46@gmail.com>
2021-12-19 11:07:45 -0300

    Segunda Etapa: Finalizando Pagina e Funções de Novo Post

--- src/pages/NewPost/index.js
+++ src/pages/NewPost/index.js
@@ -1,5 +1,9 @@
-import React, {useState, useLayoutEffect} from 'react';
+import React, {useState, useLayoutEffect, useContext} from 'react';
 import {useNavigation} from '@react-navigation/native';
+
+import storage from '@react-native-firebase/storage';
+import firestore from '@react-native-firebase/firestore'
+import {AuthContext} from '../../contexts/auth';
 
 import {Text, View} from 'react-native';
 import {Container, Input, Button, ButtonText} from './styles';
@@ -8,15 +12,53 @@ export default function NewPost() {
   const navigation = useNavigation();
   const [post, setPost] = useState('');
 
+  const {user} = useContext(AuthContext);
+
   useLayoutEffect(()=>{
     const options = navigation.setOptions({
       headerRight: () => (
-        <Button onPress={()=> alert("teste")}>
+        <Button onPress={()=> handlePost()}>
           <ButtonText>Compartilhar</ButtonText>
         </Button>
       )
     })
-  },[]);
+  },[navigation,post]);
+
+  
+  async function handlePost() {
+    if(post === ''){
+      console.log('Seu Post contem conteudo invalido.');
+      return
+    }
+    
+    let avatarUrl = null;
+    try{
+      let response = await storage().ref('users').child(user?.uid).getDownloadURL();
+      avatarUrl = response;
+    }catch(err){
+      avatarUrl = null;
+    }
+
+    await firestore().collection('posts').add({
+      created: new Date(),
+      content: post,
+      autor: user.name,
+      likes: 0,
+      avatarUrl,
+      userId: user.uid,
+    })
+    .then(()=>{
+      setPost('');
+      console.log("POST CRIADO COM SUCESSO!");
+    })
+    .catch((error)=>{
+      console.log(error);
+    })
+
+    navigation.goBack();
+
+  }
+
 
   return (
     <Container>
