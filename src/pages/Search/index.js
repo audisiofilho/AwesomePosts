0683c94b978a6bf83e7fa46b769e225d2b914ca9
audisiofilho <obuzua46@gmail.com>
2022-01-03 13:57:32 -0300

    Quinta Etapa: Buscando usuarios no banco de dados

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,13 +1,40 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Text, View} from 'react-native';
 
-import Feather from 'react-native-vector-icons/Feather';
+import firestore from '@react-native-firebase/firestore';
 
+import Feather from 'react-native-vector-icons/Feather';
 import {Container, AreaInput, Input} from './styles';
 
 export default function Search() {
   const [input, setInput] = useState('');
   const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    if (input === '' || input === undefined) {
+      setUsers([]);
+      return;
+    }
+
+    const subscriber = firestore()
+      .collection('users')
+      .where('name', '>=', input)
+      .where('name', '<=', input + '\uf8ff')
+      .onSnapshot(snapshot => {
+        const listUsers = [];
+
+        snapshot.forEach(doc => {
+          listUsers.push({
+            ...doc.data(),
+            id: doc.id,
+          });
+        });
+        setUsers(listUsers);
+        //console.log('LISTA DE USERS');
+        //console.log(listUsers);
+      });
+    return () => subscriber();
+  }, [input]);
 
   return (
     <Container>
