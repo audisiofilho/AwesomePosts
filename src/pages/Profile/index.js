8dc7b614edbcf3f6edf41acf12f12f8a50208246
audisiofilho <obuzua46@gmail.com>
2022-01-03 20:11:52 -0300

    Sexta Etapa: Criando Modal Profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, {useContext, useState} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, Modal, Platform} from 'react-native';
 
 import {AuthContext} from '../../contexts/auth';
 
@@ -14,16 +14,26 @@ import {
   UploadButton,
   UploadText,
   Avatar,
+  ModalContainer,
+  ButtonBack,
+  Input,
 } from './styles';
+
+import Feather from 'react-native-vector-icons/Feather';
 
 export default function Profile() {
   const {signOut, user} = useContext(AuthContext);
 
   const [name, setName] = useState(user?.name);
   const [url, setUrl] = useState(null);
+  const [open, setOpen] = useState(false);
 
   async function handleSignOut() {
     await signOut();
+  }
+
+  async function updateProfile(){
+    alert('teste');
   }
 
   return (
@@ -31,12 +41,12 @@ export default function Profile() {
       <Header />
 
       {url ? (
-        <UploadButton onPress={() => alert("clicou1")}>
+        <UploadButton onPress={() => alert('clicou1')}>
           <UploadText>+</UploadText>
           <Avatar source={{uri: url}} />
         </UploadButton>
       ) : (
-        <UploadButton onPress={() => alert("clicou2")}>
+        <UploadButton onPress={() => alert('clicou2')}>
           <UploadText>+</UploadText>
         </UploadButton>
       )}
@@ -44,12 +54,26 @@ export default function Profile() {
       <Name>{user?.name}</Name>
       <Email>{user?.email}</Email>
 
-      <Button bg="#428cfd">
+      <Button bg="#428cfd" onPress={() => setOpen(true)}>
         <ButtonText color="#fff">Atualizar Perfil</ButtonText>
       </Button>
       <Button bg="#ddd" onPress={handleSignOut}>
         <ButtonText color="#353840">Sair Perfil</ButtonText>
       </Button>
+
+      <Modal visible={open} animationType="slide" transparent={true}>
+        <ModalContainer behavior={Platform.OS === 'android' ? '' : 'padding'}>
+          <ButtonBack onPress={()=> setOpen(false)}>
+            <Feather name="arrow-left" size={22} color="#121212" />
+            <ButtonText color="#121212">Voltar</ButtonText>
+          </ButtonBack>
+
+          <Input placeholder={user?.name} value={name} onChangeText={(text) => setName(text)}/>
+          <Button bg="#428cfd" onPress={updateProfile}>
+            <ButtonText color="#fff">Salvar</ButtonText>
+          </Button>
+        </ModalContainer>
+      </Modal>
     </Container>
   );
 }
